1077e61be6a5baf61085e7f7dd6b97c86c50bf9f
xkazer <xkazer@sina.com>
2017-12-06 10:09:56 +0800

    添加颜色组件

---
+++ xcolor.py
@@ -0,0 +1,103 @@
+#!/usr/bin/python
+#coding:utf-8
+#author xkazer
+
+import sys, platform
+
+
+STD_INPUT_HANDLE = -10
+STD_OUTPUT_HANDLE = -11
+STD_ERROR_HANDLE = -12
+
+class XColor(object):
+    BLACK       = 0x0
+    BLUE        = 0x1
+    GREEN       = 0x2
+    RED         = 0x4
+    YELLOW      = RED | GREEN
+    PINK        = RED | BLUE
+    CYAN        = GREEN | BLUE
+    WHITE       = 0xf
+
+    _is_windows = False
+
+    def __init__(self):
+        self._is_windows = ("Windows" in platform.system())
+        if self._is_windows:
+            self._mod_ctype = __import__("ctypes")
+            self._std_out = self._mod_ctype.windll.kernel32.GetStdHandle(STD_OUTPUT_HANDLE)
+
+    def getWindowsForeColor(self, color):
+        if self.BLACK == color:     return 0x00
+        elif self.BLUE == color:    return 0x09
+        elif self.GREEN == color:   return 0x0a
+        elif self.RED == color:     return 0x0c
+        elif self.YELLOW == color:  return 0x0e
+        elif self.PINK == color:    return 0x0d
+        elif self.CYAN == color:    return 0x0b
+        else:                       return 0x0f
+
+    def getWindowsBgColor(self, color):
+        if self.WHITE == color:          return 0xf0
+        elif self.BLUE == color:         return 0x10
+        elif self.GREEN == color:        return 0x20
+        elif self.RED == color:          return 0xc0
+        elif self.YELLOW == color:       return 0xe0
+        elif self.PINK == color:         return 0xd0
+        elif self.CYAN == color:         return 0xb0
+        else:                            return 0x00 
+
+    def resetWindowsColor(self):
+        self._mod_ctype.windll.kernel32.SetConsoleTextAttribute(self._std_out, Color.WHITE)
+
+    def setWindowsColor(self, fore_color, bg_color=BLACK):
+        color = self.getWindowsForeColor(fore_color) | self.getWindowsBgColor(bg_color)
+        self._mod_ctype.windll.kernel32.SetConsoleTextAttribute(self._std_out, color)
+
+
+    def getLinuxForeColor(self, color):
+        if self.BLACK == color:     return 40
+        elif self.BLUE == color:    return 34
+        elif self.GREEN == color:   return 32
+        elif self.RED == color:     return 31
+        elif self.YELLOW == color:  return 33
+        elif self.PINK == color:    return 35
+        elif self.CYAN == color:    return 36
+        else:                       return 37
+
+    def getLinuxBgColor(self, color):
+        if self.WHITE == color:          return 47
+        elif self.BLUE == color:         return 44
+        elif self.GREEN == color:        return 42
+        elif self.RED == color:          return 41
+        elif self.YELLOW == color:       return 43
+        elif self.PINK == color:         return 45
+        elif self.CYAN == color:         return 46
+        else:                            return 40 
+
+    def setLinuxColor(self, fore_color, bg_color=BLACK):
+        sys.stdout.write("\033["+str(self.getLinuxForeColor(fore_color))+";"
+            +str(self.getLinuxBgColor(bg_color))+"m")
+
+    def resetLinuxColor(self):
+        sys.stdout.write("\033[0m")
+
+    def setColor(self, fore_color, bg_color=BLACK):
+        if self._is_windows:
+            self.setWindowsColor(fore_color, bg_color)
+        else:
+            self.setLinuxColor(fore_color, bg_color)
+
+    def resetColor(self):
+        if self._is_windows:
+            self.resetWindowsColor()
+        else:
+            self.resetLinuxColor()
+
+    def println(self, msg):
+        sys.stdout.write(msg)
+
+
+
+
+Color = XColor()
--- xfr.py
+++ xfr.py
@@ -3,6 +3,7 @@
 
 import sys, os, re
 import platform
+import logging, traceback
 from optparse import OptionParser
 
 OPTIONS={}
@@ -11,7 +12,18 @@ ARGS={}
 
 # search in file
 def searchfile(filepath):
-    global OPTIONS, ARGS
+    global OPTIONS, ARGS, gl_color
+
+    if 0 == len(ARGS):
+        if OPTIONS.color: gl_color.setColor(gl_color.BLUE)
+        print filepath
+        if OPTIONS.color: gl_color.resetColor()
+        return
+    else:
+        if "Windows" in platform.system():
+            ARGS[0] = ARGS[0].decode("gbk")
+        else:
+            ARGS[0] = ARGS[0].decode("utf-8")
 
     try:
         f = open(filepath, 'r')
@@ -24,20 +36,36 @@ def searchfile(filepath):
         for line in udata.splitlines():            
             linenum += 1
             if OPTIONS.regex:
-                pass
+                match_obj = re.search(ARGS[0], line)
+                if None == match_obj: continue
+                (pos, end) = match_obj.span()
             else:
-                if ARGS[0] in line:
-                    if OPTIONS.color:   
-                        print("\033[34;1m%s\033[0m" % filepath+" line ("+str(linenum)+"):") 
-                        print line.replace(ARGS[0], "\033[35;1m%s\033[0m"%ARGS[0])
-                    else:
-                        print filepath, "line ("+str(linenum)+"):"
-                        print line
+                pos = line.find(ARGS[0])
+                if -1 == pos: continue
+                end = pos + len(ARGS[0])
+
+            logging.debug("searchfile->result: ("+str(pos)+", "+str(end)+")")
+            if OPTIONS.color:
+                gl_color.setColor(gl_color.BLUE)
+                gl_color.println(filepath)               
+                print " line ("+str(linenum)+"):"
+                gl_color.resetColor()
+                gl_color.println(line[0:pos])                
+                gl_color.setColor(gl_color.RED)
+                gl_color.println(line[pos:end])
+                gl_color.resetColor()
+                print line[end:]
+            else:
+                print filepath, "line ("+str(linenum)+"):"
+                print line
     except Exception, e:
         if OPTIONS.verbose:
             if OPTIONS.color:
-                print("\033[34;1m%s\033[0m" % filepath)
-                print("\033[0;31m%s\033[0m" % str(e))
+                gl_color.setColor(gl_color.BLUE)
+                print filepath
+                gl_color.setColor(gl_color.RED)
+                print str(e)
+                gl_color.resetColor()
             else:
                 print filepath
                 print str(e)
@@ -46,6 +74,7 @@ def searchfile(filepath):
 def lookup():
     global OPTIONS, ARGS
 
+    logging.debug("lookup->path:"+OPTIONS.path)
     for root, dirs, files in os.walk(OPTIONS.path):
         for filename in files:
             if re.compile(OPTIONS.filter).match(filename):
@@ -53,8 +82,9 @@ def lookup():
                 searchfile(filepath)
 
 def main():
-    global OPTIONS, ARGS
+    global OPTIONS, ARGS, gl_color
 
+    logging.basicConfig(level=logging.WARN)
     usage = "usage: %prog [options] str"
     parser = OptionParser(usage)
     parser.add_option("-f", "--filter", dest="filter", default=".*",
@@ -69,15 +99,21 @@ def main():
 
     try:
         (OPTIONS, ARGS) = parser.parse_args()
-        if "Windows" in platform.system():
-            ARGS[0] = ARGS[0].decode("gbk")
-        else:
-            ARGS[0] = ARGS[0].decode("utf-8")
+        logging.debug("enter->options:"+str(OPTIONS))
+        logging.debug("enter->args:"+str(ARGS))
+        if OPTIONS.color:
+            try:
+                color_mod = __import__("xcolor")
+                gl_color = color_mod.Color
+            except:
+                OPTIONS.color = False
+        lookup()
     except Exception, e:
         parser.print_usage()
+        logging.warn("parser->err:"+str(e))
+        logging.warn("parser->"+traceback.format_exc())
         sys.exit(0)
     
-    lookup()
 
 
 
